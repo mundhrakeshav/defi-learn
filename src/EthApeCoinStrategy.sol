0111deafe5b517d28bc86f3c98c21bce6e95bec5
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-06-06 18:36:44 +0530

    Deposits

--- src/EthApeCoinStrategy.sol
+++ src/EthApeCoinStrategy.sol
@@ -5,6 +5,8 @@ import {IPool} from "./AAVE/IPool.sol";
 import {IWETH9} from "./IWETH9.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {Owned} from "solmate/auth/Owned.sol";
+import {ISwapRouter} from "./Uniswap/ISwapRouter.sol";
+import {IHelperContractParaSpace} from "./IHelperContractParaSpace.sol";
 
 // Precision for price in USD is 1e8
 contract EthApeCoinStrategy is Owned, ERC20("EACS", "EACS", 18) {
@@ -12,12 +14,19 @@ contract EthApeCoinStrategy is Owned, ERC20("EACS", "EACS", 18) {
     address public wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public swapRouterAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address public apeCoinAddress = 0x4d224452801ACEd8B2F0aebE155379bb5D594381;
+    address public helperContractParaSpaceAddress = 0xBAa0DaA4224d2eb4619FfDC8A50Ef50c754b55F3;
+
     IPool public aavePool = IPool(aavePoolAddress);
     IWETH9 public weth = IWETH9(wethAddress);
     ERC20 public usdc = ERC20(usdcAddress);
     ERC20 public dai = ERC20(daiAddress);
+    ERC20 public ape = ERC20(apeCoinAddress);
+    ISwapRouter public swapRouter = ISwapRouter(swapRouterAddress);
+    IHelperContractParaSpace public helperContractParaSpace = IHelperContractParaSpace(helperContractParaSpaceAddress);
 
-    address public admin;
+    uint24 public constant poolFee = 3000;
 
     constructor(address _owner) Owned(_owner) {}
 
@@ -35,4 +44,28 @@ contract EthApeCoinStrategy is Owned, ERC20("EACS", "EACS", 18) {
         aavePool.borrow(_asset, _amount, 2, 0, address(this));
     }
 
+    function swapUSDCForApe() external onlyOwner {
+        uint256 _amt = usdc.balanceOf(address(this));
+        //
+        usdc.approve(swapRouterAddress, _amt);
+        //
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
+            tokenIn: usdcAddress,
+            tokenOut: apeCoinAddress,
+            fee: poolFee,
+            recipient: address(this),
+            deadline: block.timestamp,
+            amountIn: _amt,
+            amountOutMinimum: 0,
+            sqrtPriceLimitX96: 0
+        });
+        //
+        swapRouter.exactInputSingle(params);
+    }
+    function swapApeForPCAPE() external onlyOwner {
+        uint256 _amt = ape.balanceOf(address(this));
+        ape.approve(helperContractParaSpaceAddress, type(uint).max);
+
+        helperContractParaSpace.convertApeCoinToPCApe(_amt);
+    }
 }
--- test/EthApeCoinStrategy.t.sol
+++ test/EthApeCoinStrategy.t.sol
@@ -5,6 +5,7 @@ import {BaseTest} from "./Base.sol";
 import {EthApeCoinStrategy} from "../src/EthApeCoinStrategy.sol";
 import {DataTypes} from "../src/AAVE/DataTypes.sol";
 import {IPool} from "../src/AAVE/IPool.sol";
+import {ISwapRouter} from "../src/Uniswap/ISwapRouter.sol";
 import {IWETH9} from "../src/IWETH9.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
@@ -13,11 +14,17 @@ contract EthApeCoinStrategyTest is BaseTest {
     address public wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address public usdcAddress = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     address public daiAddress = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public oneInchAddress = 0x111111111117dC0aa78b770fA6A738034120C302;
+    address public apeCoinAddress = 0x4d224452801ACEd8B2F0aebE155379bb5D594381;
+    address public swapRouterAddress = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
+    address public pcApeAddress = 0xDDDe38696FBe5d11497D72d8801F651642d62353;
+
     IPool public aavePool = IPool(aavePoolAddress);
     IWETH9 public weth = IWETH9(wethAddress);
     ERC20 public usdc = ERC20(usdcAddress);
     ERC20 public dai = ERC20(daiAddress);
+    ERC20 public ape = ERC20(apeCoinAddress);
+    ERC20 public pcApe = ERC20(pcApeAddress);
+    ISwapRouter public swapRouter = ISwapRouter(swapRouterAddress);
 
     EthApeCoinStrategy public strategy;
     address depositor = makeAddr("DEPOSITOR");
@@ -31,30 +38,26 @@ contract EthApeCoinStrategyTest is BaseTest {
     function testDeposit() public {
         hoax(admin);
         strategy.approveToken(ERC20(wethAddress), aavePoolAddress, type(uint256).max);
+        // Deposit
         hoax(depositor);
         strategy.deposit{value: 1 ether}();
-        DataTypes.ReserveData memory _reserveData = aavePool.getReserveData(wethAddress);
-        ERC20 _aToken = ERC20(_reserveData.aTokenAddress);
-        (
-            uint256 totalCollateralBase,
-            uint256 totalDebtBase,
-            uint256 availableBorrowsBase,
-            uint256 currentLiquidationThreshold,
-            uint256 ltv,
-            uint256 healthFactor
-        ) = aavePool.getUserAccountData(address(strategy));
+        // DataTypes.ReserveData memory _reserveData = aavePool.getReserveData(wethAddress);
+        // ERC20 _aToken = ERC20(_reserveData.aTokenAddress);
+        (,, uint256 availableBorrowsBase,,,) = aavePool.getUserAccountData(address(strategy));
         logUserAccountData(address(strategy), string("Deposited"));
-        //
+        // Borrow
         hoax(admin);
         strategy.borrowFromAAVE(usdcAddress, changeScaleUSDC(availableBorrowsBase) / 2);
         logUserAccountData(address(strategy), string("Borrowed"));
+        // Swap
+        hoax(admin);
+        strategy.swapUSDCForApe();
+        emit log_uint(ape.balanceOf(address(strategy)));
         //
-
-        usdc.approve(oneInchAddress, type(uint).max);
-        emit log_uint(usdc.balanceOf(address(strategy)));
-        emit log_address(address(strategy));
-        // (bool success, bytes memory data) = oneInchAddress.call("0x095ea7b30000000000000000000000001111111254eeb25477b68fb85ed929f73a960582ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
-        // assertTrue(success);
+        hoax(admin);
+        strategy.swapApeForPCAPE();
+        emit log_uint(ape.balanceOf(address(strategy)));
+        emit log_uint(pcApe.balanceOf(address(strategy)));
     }
 
     function logUserAccountData(address _user, string memory _details) private {
@@ -80,8 +83,4 @@ contract EthApeCoinStrategyTest is BaseTest {
     function changeScaleUSDC(uint256 _amt) internal pure returns (uint256) {
         return (_amt * 1e6) / 1e8;
     }
-    // function testSetNumber(uint256 x) public {
-    //     counter.setNumber(x);
-    //     assertEq(counter.number(), x);
-    // }
 }
