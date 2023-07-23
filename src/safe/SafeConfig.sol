4ff619fadb51d1822b1f5e86d03794236723a00c
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-07-23 18:16:01 +0530

    Add Safe

---
+++ src/safe/SafeConfig.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+
+abstract contract SafeConfig {
+    address constant safeSingleton = 0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552;
+    address constant safeFactory = 0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2;
+}
---
+++ src/safe/SafeFactory/IProxyCreationCallback.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity 0.8.18;
+import "./GnosisSafeProxy.sol";
+
+interface IProxyCreationCallback {
+    function proxyCreated(GnosisSafeProxy proxy, address _singleton, bytes calldata initializer, uint256 saltNonce)
+        external;
+}
